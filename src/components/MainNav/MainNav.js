7dd91b4f0ff69691e997251ceeea913019b2c633
Michaela Lehr <>
2018-07-28 16:36:10 +0200

    #7 get session state in main nav

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+
+import { initialiseSession } from '../../containers/Session/SessionActions'
 
 class MainNav extends Component {
   render() {
+    const { sessionState } = this.props
+
     return(
       <nav>
         <ol>
@@ -10,11 +16,41 @@ class MainNav extends Component {
           <li><Link to="/podcasterinnen">Podcasterinnen</Link></li>
           <li><Link to="/faq">FAQ</Link></li>
           <li><Link to="/about">Über uns</Link></li>
-          <li><Link to="/session">Login/Register</Link></li>
+          <li>
+            <Link to="/session">
+              { sessionState === 'UNKNOWN' &&
+                <span>Login</span>
+              }
+              { sessionState === 'REGISTERED' &&
+                <span>Register</span>
+              }
+            </Link>
+          </li>
         </ol>
       </nav>
     )
   }
 }
 
-export default MainNav
+MainNav.propTypes = {
+  sessionState: PropTypes.string,
+}
+
+MainNav.defaultProps = {
+  sessionState: 'UNKNOWN',
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  handleInitSession: () => {
+    dispatch(initialiseSession())
+  },
+})
+
+const mapStateToProps = (state) => ({
+  sessionState: state.sessionReducer.sessionState,
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(MainNav)
