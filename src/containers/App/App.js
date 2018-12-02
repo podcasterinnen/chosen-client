edc8dcfa9248afcdf54c30772905d0d805f21c18
DESKTOP-COK2JP7\Daniela <daniela@diehorst.de>
2018-12-02 12:19:35 +0100

    Correct the text for the landing page

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <h1 className="title app__headline">podcasterinnen.org</h1>
         <p className="subtitle">Nicht-binäre Podcastende und Technik. Podcasterinnen und Inhalte. Podcasterinnen und nicht-binäre Podcastende hinter dem Mikro und auf der Bühne.</p>
         <Link className="button button--cta app__button" to="/session">Mitmachen</Link>
-        <p>Podcasterinnen und nicht-binäre Podcastende gibt es viel, doch leider sind sie viel zu selten zu Gast auf Bühnen, in Podcasts oder bei Workshops. <br/>Du bist Podcaster:in und suchst für ein Thema eine Gästin? Du bist Veranstalter:in und suchst nicht-binäre Podcastende oder eine Podcasterin für einen Vortrag oder ein Workshop? Bestimmt findest du sie hier:</p>
+        <p>Podcasterinnen und nicht-binäre Podcastende gibt es viel, doch leider sind sie selten zu Gast auf Bühnen, in Podcasts oder bei Workshops. <br/>Du bist Podcaster:in und suchst für ein Thema eine Gästin? Du bist Veranstalter:in und suchst nicht-binäre Podcastende oder eine Podcasterin für einen Vortrag oder ein Workshop? Bestimmt findest du sie hier:</p>
       </section>
     )
   }
