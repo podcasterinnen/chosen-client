70c23f4f3fdd05ce56de00f27e0b1359b893c70a
Michaela Lehr <michi_lehr@web.de>
2018-12-04 16:29:53 +0100

    #127 MOD layout landing page

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -25,29 +25,33 @@ class App extends Component {
     const { podcasterinnen } = this.props
 
     return (
-      <section className="app main__section">
-        <h1 className="title app__headline">podcasterinnen.org</h1>
-        <p className="subtitle app__subtitle">Nicht-binäre Podcastende und Technik. Podcasterinnen und Inhalte. Podcasterinnen und nicht-binäre Podcastende hinter dem Mikro und auf der Bühne.</p>
-        <p className="app__cta"><Link className="button button--cta app__button" to="/session">Mitmachen</Link></p>
-        <p>Podcasterinnen und nicht-binäre Podcastende gibt es viel, doch leider sind sie selten zu Gast auf Bühnen, in Podcasts oder bei Workshops.<br/>Du bist Podcaster:in und suchst für ein Thema eine Gästin? Du bist Veranstalter:in und suchst nicht-binäre Podcastende oder eine Podcasterin für einen Vortrag oder ein Workshop? Bestimmt findest du sie hier.</p>
-        { podcasterinnen &&
-        <div>
-          <h2>Neueste Podcasterinnen-Profile:</h2>
-          <ul className="podcasterinnen__list">
-            { podcasterinnen.sort(this.sortPodcasterinnen).map((podcasterin, i) => {
-              if (podcasterin.profile_state === 'PUBLISHED' && i < 4) {
-                return (
-                  <li className="podcasterinnen__list__item" key={generateKey(podcasterin.forename, i)}>
-                    <PodcasterinnenCard item={podcasterin}></PodcasterinnenCard>
-                  </li>
-                )
-              }
-            })}
-          </ul>
-          <p className="app__cta"><Link className="button button--cta app__button" to="/podcasterinnen">Weitere Podcasterinnen</Link></p>
-        </div>
-        }
-      </section>
+      <div>
+        <section className="app-cta main__section">
+          <h1 className="title app__headline">podcasterinnen.org</h1>
+          <p className="subtitle app__subtitle">Nicht-binäre Podcastende und Technik. Podcasterinnen und Inhalte. Podcasterinnen und nicht-binäre Podcastende hinter dem Mikro und auf der Bühne.</p>
+          <p className="app__cta"><Link className="button button--cta app__button" to="/session">Mitmachen</Link></p>
+          <p className="app-cta__text">Podcasterinnen und nicht-binäre Podcastende gibt es viel, doch leider sind sie selten zu Gast auf Bühnen, in Podcasts oder bei Workshops. Du bist Podcaster:in und suchst für ein Thema eine Gästin? Du bist Veranstalter:in und suchst nicht-binäre Podcastende oder eine Podcasterin für einen Vortrag oder ein Workshop? Bestimmt findest du sie hier.</p>
+        </section>
+        <section className="app-copy">
+          { podcasterinnen &&
+          <div>
+            <h2>Neueste Podcasterinnen-Profile:</h2>
+            <ul className="podcasterinnen__list">
+              { podcasterinnen.sort(this.sortPodcasterinnen).map((podcasterin, i) => {
+                if (podcasterin.profile_state === 'PUBLISHED' && i < 4) {
+                  return (
+                    <li className="podcasterinnen__list__item" key={generateKey(podcasterin.forename, i)}>
+                      <PodcasterinnenCard item={podcasterin}></PodcasterinnenCard>
+                    </li>
+                  )
+                }
+              })}
+            </ul>
+            <p className="app__cta"><Link className="button button--cta app__button" to="/podcasterinnen">Weitere Podcasterinnen</Link></p>
+          </div>
+          }
+        </section>
+      </div>
     )
   }
 }
