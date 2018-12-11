d786d058b4157c3b7a34738e464d8f60b4b7685e
Michaela Lehr <michi_lehr@web.de>
2018-12-11 16:41:07 +0100

    MOD about layout, content

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -17,38 +17,44 @@ class About extends Component {
         <h3>(Gehör) finden und gefunden werden</h3>
         <p>Dazu könnt ihr, als Podcasterin/nicht-binär Podcastende, euch auf dieser Webseite ein aussagekräftiges Profil anlegen und damit euch und eure Podcasts präsentieren. Besucher:innen von <a href="https://podcasterinnen.org">podcasterinnen.org</a> können hier nicht nur neue Formate entdecken, sondern auch Impulse und Ideen, z. B. für Veranstaltungen oder einzelne Podcast-Episoden mitnehmen. Die Seite richtet sich also auch an Podcaster:innen, die auf der Suche nach passenden Gesprächspartnerinnen sind, oder Veranstalter:innen, die mit Expertinnen zu bestimmten Themengebieten in Kontakt treten möchten.</p>
         <h2>Das Team hinter podcasterinnen.org sind:</h2>
-        <div>
-          <h3>Daniela Ishorst – die Podcasterin.</h3>
-          <p>Danielas Themenschwerpunkte im Podcast-Bereich sind Theater, Kunst und Literatur. Sie setzt sich mit podcasterinnen.org dafür ein, dass Frauen in Podcasts sowie bei Veranstaltungen zum Thema Podcasting sichtbarer werden.<br />Bei podcasterinnen.org arbeitet sie an den Inhalten, dem Design und der Organisation der Abläufe.</p>
-          <ul>
-            <li>Twitter: <a href="https://twitter.com/die_horst" rel="noopener noreferrer" target="_blank">@die_horst</a></li>
-            <li>Podcast: <a href="http://www.kunstundhorst.de" rel="noopener noreferrer" target="_blank">kunst & horst</a></li>
-          </ul>
+        <div className="founder">
+          <div className="founder__imagecontainer">
+            <img className="founder__imagecontainer__image" src={photoDaniela} alt="Daniela Ishorst" />
+          </div>
+          <div>
+            <h3>Daniela Ishorst – die Podcasterin.</h3>
+            <p>Danielas Themenschwerpunkte im Podcast-Bereich sind Theater, Kunst und Literatur. Sie setzt sich mit podcasterinnen.org dafür ein, dass Frauen in Podcasts sowie bei Veranstaltungen zum Thema Podcasting sichtbarer werden.<br />Bei podcasterinnen.org arbeitet sie an den Inhalten, dem Design und der Organisation der Abläufe.</p>
+            <ul>
+              <li>Twitter: <a href="https://twitter.com/die_horst" rel="noopener noreferrer" target="_blank">@die_horst</a></li>
+              <li>Podcast: <a href="http://www.kunstundhorst.de" rel="noopener noreferrer" target="_blank">kunst & horst</a></li>
+            </ul>
+          </div>
         </div>
-        <div className="founder__imagecontainer">
-          <img className="founder__imagecontainer__image" src={photoDaniela} alt="Daniela Ishorst" />
+        <div className="founder">
+          <div className="founder__imagecontainer">
+            <img className="founder__imagecontainer__image "src={photoMichaela} alt="Michaela Lehr" />
+          </div>
+          <div>
+            <h3>Michaela Lehr – die Entwicklerin.</h3>
+            <p>Michaela gründete 2012 ihrer eigenen Firma Geil,Danke!, in der sie seitdem als Softwareentwicklerin arbeitet. Sie designt, programmiert und entwickelt eure Webseite und Datenbank, prüft eine Menge Code und hält die technischen Möglichkeiten im Blick.</p>
+            <ul>
+              <li>Twitter: <a href="https://twitter.com/fischaelameer" rel="noopener noreferrer" target="_blank">@fischaelameer</a></li>
+              <li>Webseite: <a href="https://geildanke.com" rel="noopener noreferrer" target="_blank">geildanke.com</a></li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <h3>Michaela Lehr – die Entwicklerin.</h3>
-          <p>Michaela gründete 2012 ihrer eigenen Firma geildanke.com, in der sie seitdem als Softwareentwicklerin arbeitet. In ihrer Freizeit unterstützt sie das Open Source-Projekt Podlove.<br />Sie programmiert und entwickelt eure Datenbank, prüft eine Menge Code und hält die technischen Möglichkeiten und Komponenten im Blick.</p>
-          <ul>
-            <li>Twitter: <a href="https://twitter.com/fischaelameer" rel="noopener noreferrer" target="_blank">@fischaelameer</a></li>
-            <li>Webseite: <a href="https://geildanke.com" rel="noopener noreferrer" target="_blank">geildanke.com</a></li>
-          </ul>
-        </div>
-        <div className="founder__imagecontainer">
-          <img className="founder__imagecontainer__image "src={photoMichaela} alt="Michaela Lehr" />
-        </div>
-        <div>
-          <h3>Nele Heise – die Stimme aus dem Off.</h3>
-          <p>Nele ist Medienforscherin und beschäftigt sich seit 2013 mit der deutschsprachigen Podcast-Landschaft. Ein Tweet war Anstoß zur “Frauenstimmen im Netz”-Liste, auf der sie seit vier Jahren Podcasts von und mit Frauen sammelt und kuratiert. Dass aus diesem Impuls nun podcasterinnen.org entsteht, findet Nele “mega gut”.<br />Sie unterstützt Daniela und Michaela dabei, den Überblick über die vielfältigen Themen und Formate von Podcasterinnen zu behalten. Außerdem hat sie ein Auge darauf, dass Texte und redaktionelle Inhalte fehlerfrei auf unserer Webseite landen.</p>
-          <ul>
-            <li>Twitter: <a href="https://twitter.com/neleheise" rel="noopener noreferrer" target="_blank">@neleheise</a></li>
-            <li>Webseite: <a href="http://www.neleheise.de/" rel="noopener noreferrer" target="_blank">neleheise</a></li>
-          </ul>
-        </div>
-        <div className="founder__imagecontainer">
-          <img className="founder__imagecontainer__image"src={photoNele} alt="Nele Heise" />
+        <div className="founder">
+          <div className="founder__imagecontainer">
+            <img className="founder__imagecontainer__image"src={photoNele} alt="Nele Heise" />
+          </div>
+          <div>
+            <h3>Nele Heise – die Stimme aus dem Off.</h3>
+            <p>Nele ist Medienforscherin und beschäftigt sich seit 2013 mit der deutschsprachigen Podcast-Landschaft. Ein Tweet war Anstoß zur “Frauenstimmen im Netz”-Liste, auf der sie seit vier Jahren Podcasts von und mit Frauen sammelt und kuratiert. Dass aus diesem Impuls nun podcasterinnen.org entsteht, findet Nele “mega gut”.<br />Sie unterstützt Daniela und Michaela dabei, den Überblick über die vielfältigen Themen und Formate von Podcasterinnen zu behalten. Außerdem hat sie ein Auge darauf, dass Texte und redaktionelle Inhalte fehlerfrei auf unserer Webseite landen.</p>
+            <ul>
+              <li>Twitter: <a href="https://twitter.com/neleheise" rel="noopener noreferrer" target="_blank">@neleheise</a></li>
+              <li>Webseite: <a href="http://www.neleheise.de/" rel="noopener noreferrer" target="_blank">neleheise</a></li>
+            </ul>
+          </div>
         </div>
       </section>
     )
