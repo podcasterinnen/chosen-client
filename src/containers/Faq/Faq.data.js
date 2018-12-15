dcc0b098ead5681ce893b5cc1b83d3abe5b1a97f
Michaela Lehr <michi_lehr@web.de>
2018-12-15 18:37:01 +0100

    MOF move content from faq container

---
+++ src/containers/Faq/Faq.data.js
@@ -0,0 +1,33 @@
+export const content = [{
+  copy: 'Jede nicht-binäre Podcastperson/Podcasterin, die einen eigenen Podcast produziert und/oder in einem festen Team podcastet, kann sich bei uns ein Profil anlegen.',
+  index: 0,
+  headline: 'Wer kann sich bei podcasterinnen.org ein Profil anlegen?',
+}, {
+  copy: 'Zuerst musst du dich mit deiner E-Mail-Adresse und einem <a href="http://www.sicherespasswort.com/" rel="noopener noreferrer" target="_blank">(möglichst sicheren)</a> Passwort registrieren. Du bekommst eine Bestätigungsmail, die dich auf die Website <a href="https://podcasterinnen.org">podcasterinnen.org</a> zurückführt. Nun kannst du dich erneut anmelden und mit dem Anlegen deines Profils beginnen.',
+  index: 1,
+  headline: 'Wie melde ich mich bei podcasterinnen.org an?',
+}, {
+  copy: 'Welche Informationen du in deinem Profil angeben möchtest, ist letztlich ganz dir überlassen. Wir empfehlen dir aber, die wichtigsten Schlagworte für deinen Podcast zu verwenden, damit die Besucher:innen von podcasterinnen.org dich bzw. deine Podcasts schnell finden können.<br/>Du kannst dein Profil jederzeit weiter bearbeiten und verändern. Eine ausführliche Anleitung zum Anlegen und Verwalten deines Profils findest du weiter unten („In wenigen Schritten zu deinem Profil“).',
+  index: 2,
+  headline: 'Wie sieht ein gut ausgefülltes Profil aus?',
+}, {
+  copy: 'Es kann ein paar Tage dauern, bis wir dein Profil freigeschaltet haben, da wir ein sehr kleines Team sind.',
+  index: 3,
+  headline: 'Wann wird mein Profil freigeschaltet?',
+}, {
+  copy: 'Jede Podcastperson kann sich ihr persönliches Profil anlegen. Allerdings gibt es einen Ausschluss: Wir werden keinen Menschen bei uns eine Plattform bieten, die aus dem rechten Spektrum (AfD, Identitäre Bewegung, Pegida etc.) kommen oder mit ihrem Podcast rechte oder antisemitische Hetze verbreiten. Menschenverachtung, Sexismus und rechter Hetze werden wir hier keinen Raum geben. Sollte dir ein solches Profil auffallen, melde dich bitte bei uns: <a href="mailto:contact@podcasterinnen.org">contact@podcasterinnen.org</a>.',
+  index: 4,
+  headline: 'Warum ist es überhaupt nötig mein Profil freischalten zu lassen?',
+}, {
+  copy: 'Wenn du dein Profil löschen möchtest, kannst du uns gerne eine E-Mail an <a href="mailto:contact@podcasterinnen.org">contact@podcasterinnen.org</a> schicken. Wir führen die Löschung des Profils so schnell als möglich durch.',
+  index: 5,
+  headline: 'Wie kann ich mein Profil löschen?',
+}, {
+  copy: 'Das hat vor allem etwas mit der Übersichtlichkeit der Seite zu tun. Wenn du dein Profil einrichtest, kannst du deinem Podcast verschiedenen Kategorien zuordnen. So wissen Besucher:innen deines Profils gleich, um welche Themen sich dein Format inhaltlich dreht und zu welchen Themen du gerne sprechen möchtest. Besucher:innen können nach Schlagworten suchen und finden sofort die passenden Profile zu ihrem Thema.</p><p>Damit es schön praktisch und übersichtlich bleibt, haben wir uns für so genannte „hard coded tags” entschieden. Das heißt, die vorgegebenen Kategorien sind fix und können von dir nicht verändert oder um neue Schlagworte erweitert werden. Dadurch findest du vielleicht nicht die eine Kategorie bzw. „tags“, die dein Format perfekt beschreiben. Aber es erleichtert die Bedienung der Seite immens und stellt ohnehin nur eine erste grobe Einordnung deines Formats dar. In der Beschreibung auf deinem Profil kannst du nämlich noch viel spezieller und genauer auf dich und die Inhalte deines Podcasts eingehen. Wenn du findest, dass wir (trotz intensiver Recherche) eine ganz wichtige Themen-Kategorie vergessen haben und unbedingt ergänzen sollten, dann schreibe uns doch einfach <a href="https://github.com/podcasterinnen/chosen-client/issues/161" target="_blank">hier auf Github einen Kommentar</a>.',
+  index: 6,
+  headline: 'Warum gibt es meine Kategorie (meinen Tag, mein Schlagwort) nicht?',
+}, {
+  copy: 'In erster Linie lebt eine solche Webseite davon, dass du sie nutzt und die Idee dahinter in die Welt trägst. Also: Schau dich um, vernetze dich und supporte andere Podcastpersonen und Podcasterinnen. Erzähl anderen, dass es diese Webseite gibt und warum sie sich hier anmelden sollten.</p><p>Wir freuen uns über jedwede Form der ideellen Unterstützung. Erwähne podcasterinnen.org in deinen Podcasts. Schreib über uns. Erzähl Veranstalter:innen und allen, die podcastende Expert*innen zu einer riesigen Vielfalt an Themen suchen, dass sie hier fündig werden. Sharing is caring.</p><p><a href="https://podcasterinnen.org">podcasterinnen.org</a> ist ein Open-Source-Projekt und kann immer auch technische Unterstützung gebrauchen - im Großen wie im Kleinen. Du kannst entwickeln/programmieren und möchtest einen Beitrag zu podcasterinnen.org leisten? Auch kleine Bausteine können uns schon weiterhelfen. Also nur Mut - melde dich gerne via <a href="https://twitter.com/podcasterinnen" target="_blank">Twitter</a> oder <a href="mailto:contact@podcasterinnen.org">E-Mail</a>, wir freuen uns. Natürlich kannst du auch direkt mit Programmieren loslegen. Den Sourcecode für das <a href="https://github.com/podcasterinnen/chosen-api" target="_blank">Backend</a> und das <a href="https://github.com/podcasterinnen/chosen-client" target="_blank">Frontend</a> findest du auf unserer <a href="https://github.com/podcasterinnen/" target="_blank">Github-Seite</a>. Wir freuen uns über Bug-Tickets, Verbesserungsvorschläge und Pull-Requests.',
+  index: 7,
+  headline: 'Wie kann ich euch unterstützen?',
+}]
